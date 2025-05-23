6dfba11ff1bae115b9c99fa9009ac230a5a258c0
LuizAragao96 <169122380+LuizAragao96@users.noreply.github.com>
2025-05-23 18:47:56 -0300

    Add files via upload

---
+++ Conexao.java
@@ -0,0 +1,19 @@
+public class Conexao {
+
+    private versão mais;
+    private Use a;
+    public Use getA() {
+        Use a = null;
+        return a;
+    }
+    public void setA(Use a) {
+        this.a = a;
+    }
+    public versão getMais() {
+        return mais;
+    }
+    public void setMais(versão mais) {
+        this.mais = mais;
+    } 
+
+}
---
+++ versão.java
@@ -0,0 +1,4 @@
+
+public class versão {
+
+}
